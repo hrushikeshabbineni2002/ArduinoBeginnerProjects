4ea22f9daf81b390260fe24e9c40a1b93de9025a
sp1004 <saurav.flyers@gmail.com>
2019-08-12 16:20:37 +0530

    A better way

---
+++ Bi0sTask1 Challenges/Password/Password.ino
@@ -0,0 +1,32 @@
+String password ;
+String correctpassword = String("1234");
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if (Serial.available()>0)
+  {
+    if (check() == true)
+      Serial.print("Correct Password");
+    else
+      Serial.print("Incorrect Password");
+    while(1){}
+  }
+}
+
+bool check()
+{
+    password = Serial.readString();
+    if (password.equals(correctpassword))
+    { 
+      return true ;
+    }
+    else
+    {
+      return false ; 
+    }
+}
